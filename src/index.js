0bfb12ce7dfb457d6d8abc065eb225ba85d2091a
James McKeag <mckeag.james@yahoo.com>
2023-06-09 16:18:11 -0600

    began work on routing and css. installed react-router-dom to establish routes from homepage.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
-import {BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const container= document.getElementById('root');
 const root = ReactDOM.createRoot(container);
