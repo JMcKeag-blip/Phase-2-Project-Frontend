710d9ae1ce23b43d7ee8b49c3217556363878c8d
James McKeag <mckeag.james@yahoo.com>
2023-06-09 15:58:10 -0600

    Added Home.js component and starting css to get home page displayed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useState} from "react";
-
+import Home from "./Home";
 function App() {
+  return(
+    <>
+      <h1 id="main-header"> Planet Explorer</h1>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  )
 }
 
 export default App;
