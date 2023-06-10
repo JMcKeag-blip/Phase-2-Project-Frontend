cd66d403d2d3ff1e813191b5eea1480964346b6c
James McKeag <mckeag.james@yahoo.com>
2023-06-09 18:14:58 -0600

    added useEffect and useState for initial loads of backend

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,16 @@ import Navbar from "./Navbar";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const [initialPlanetList, setInitialPlanetList] = useState([])
+
+const LOCAL= "http://localhost:3001/planets"
+
+useEffect(()=> {
+  fetch(LOCAL)
+  .then(r => r.json())
+  .then(d => setInitialPlanetList(d))
+}, [])
+
   return(
     <>
       <h1 id="main-header"> Planet Explorer</h1>
