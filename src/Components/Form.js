3c4e503ffc88731a2c849a2c2ea762c4f02980ba
James McKeag <mckeag.james@yahoo.com>
2023-06-23 18:52:20 -0600

    added fillable form for value fields

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Form({ onHandleSubmit }) {
     const [formData, setFormData] = useState({
         name: "",
-        gravitiy: "",
+        gravity: "",
         population: "",
         climate: "",
         terrain: ""
@@ -11,12 +11,12 @@ function Form({ onHandleSubmit }) {
     const dataToPost = {
         "index": `HB ${formData.name}`,
         "name": formData.name,
-        "gravitiy": formData.gravitiy,
+        "gravity": formData.gravity,
         "population": formData.population,
         "climate": formData.climate,
         "terrain": formData.terrain
     }
-}
+
 
 function handleChange(e){
     const location = e.target.name
@@ -30,7 +30,7 @@ function handleChange(e){
 
 function handleSubmit(e){
     e.preventDefault()
-    if(formData.name === '' || formData.gravitiy === '' || formData.terrain === ''){
+    if(formData.name === '' || formData.gravity === '' || formData.terrain === ''){
         return window.alert('Please fill in all required forms.')
     }
     window.alert(`${formData.name}, has been added to the Itinerary.`)
@@ -38,11 +38,72 @@ function handleSubmit(e){
     setFormData({
         "index": `HB ${formData.name}`,
         "name": formData.name,
-        "gravitiy": formData.gravitiy,
+        "gravity": formData.gravity,
         "population": formData.population,
         "climate": formData.climate,
         "terrain": formData.terrain
     })
+}
+
+return (
+    <div className="form-container">
+        <p id="form-info">
+            Here you can enter in your planets to the Itinerary!
+            <br/>
+            Any form elements that are bolded and or have an asterisk next to the title of the field
+            are required elements for the form to be able to be submitted.
+            <br/>
+            Any custom planets will have a special category added to their cards in the itinerary.
+            Also these planets may display some empty information to help distinguish them as a custom planet.
+        </p>
+        <form>
+            <strong>*Planet Name </strong>
+            <input
+                type='text'
+                name='name'
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Planet name..">
+            </input>
+            <br/>
+            <strong>*Planet Gravity </strong>
+            <input
+                type='text'
+                name='gravity'
+                value={formData.gravity}
+                onChange={handleChange}
+                placeholder="Planet Gravity..">
+            </input>
+            <br/>
+            <strong>*Planet Terrain</strong>
+            <input
+                type='text'
+                name='terrain'
+                value={formData.terrain}
+                onChange={handleChange}
+                maxLength='100'
+                placeholder='Description of terrain.. max characters 100..'>
+            </input>
+            <p>Planet Population</p>
+            <input
+                type='text'
+                name='population'
+                value={formData.population}
+                onChange={handleChange}
+                placeholder='Planet population number'>
+            </input>
+            <br/>
+            <p>Planet Climate</p>
+            <input
+                type='text'
+                name='climate'
+                value={formData.climate}
+                onChange={handleChange}
+                placeholder='Planetary Climate(s)'>
+            </input>
+        </form>
+    </div>
+    )
 }
 
 export default Form
